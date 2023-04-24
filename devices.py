5538d25616eae853d71252ee15ca81cc8b43be2e
HF-biu <1564598834@qq.com>
2023-04-24 11:01:40 +0800

    add some devices and rooms

---
+++ devices.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+
+import math
+import pygame
+from sqlManage import SQLManagement
+
+class device:
+
+	typeStrToNum =        {
+		"lamp"           :        0,
+		"airCondition"   :        1,
+		"TV"             :        2,
+		"computer"       :        3,
+		"charger"        :        4,
+		"heater"         :        5,
+		"door"           :        6,
+		"window"         :        7,
+		"other"          :        8,
+        
+        #new
+        "audio"          :        9,
+        "switch"         :        10,
+        "camere"         :        11,
+        "thermometer"    :        12,
+        "sensor"         :        13,
+        "socket"         :        14
+	}
+
+	# typeNumToStr =        {
+	# 	0                :        "lamp",
+	# 	1                :        "airCondition",
+	# 	2                :        "TV",
+	# 	3                :        "computer",
+	# 	4                :        "charger",
+	# 	5                :        "heater",
+	# 	6                :        "door",
+	# 	7                :        "window",
+	# 	8                :        "other"
+	# }       
+
+	def __init__(self, code = 0, deviceName = 'None', deviceType = 'other', statu = 0, value = -1, posX = 0, posY = 0, ID = -1):
+		self.code        =  code
+		self.name        =  deviceName
+		self.deviceType  =  deviceType
+		self.statu       =  statu
+		self.value       =  value
+		self.posX        =  posX
+		self.posY        =  posY
+		self.ID          =  -1
+		self.imageOn     =  pygame.image.load('./pic/' + self.getType() + '_on.png')
+		self.imageOff    =  pygame.image.load('./pic/' + self.getType() + '_off.png')
+
+
+	def getCode(self):
+		return self.code
+
+	def setCode(self, code):
+		self.code = code
+
+	def setID(self, ID):
+		self.ID = ID
+
+	def getID(self):
+		return self.ID
+
+	def getName(self):
+		return self.name
+
+	def setName(self, deviceName):
+		self.name = deviceName
+
+	def getStatu(self):
+		if(self.statu == 0):
+			return False
+		else:
+			return True
+
+	def turnOn(self):
+		self.statu        =  1
+		# print(self.name + ' has been turn on')
+
+	def turnOff(self):
+		self.statu        =  0
+		# print(self.name + ' has been turn off')
+
+	def getValue(self):
+		return self.value
+
+	def setValue(self, value):
+		self.value        =  value
+
+	def getType(self):
+		return self.deviceType
+
+	def setType(self, Type):
+		if(self.typeStrToNum.get(Type) == 'None'):
+			self.deviceType    =  'other'
+		else:
+			self.deviceType    =  Type
+
+	def setPos(self, posX, posY):
+		self.posX         =  posX
+		self.posY         =  posY
+
+	def getPosX(self):
+		return self.posX
+
+	def getPosY(self):
+		return self.posY
+
+	def getImage(self):
+		if(self.getStatu()):
+			return self.imageOn
+		else:
+			return self.imageOff
+
+	def calDistance(self, posX, posY):
+		dx = self.posX - posX
+		dy = self.posY - posY
+		dis = math.sqrt(dx*dx + dy*dy)
+		return dis
+
+	def setRandomDevicePos(self, roomLeft, roomRight, roomTop, roomBottom):
+		import random
+		if(self.deviceType == 'lamp'):
+			tempPosX = int((roomLeft+roomRight)/2)
+			tempPosY = int((roomTop+roomBottom)/2)
+			self.setPos(tempPosX, tempPosY)
+			# print('lamp', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'airCondition'):
+			direction = random.randint(1, 4)
+			if(direction == 1):
+				tempPosX = roomLeft
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 2):
+				tempPosX = roomRight
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 3):
+				tempPosY = roomTop
+				tempPosX = random.randint(roomLeft, roomRight)
+			if(direction == 4):
+				tempPosY = roomBottom
+				tempPosX = random.randint(roomLeft, roomRight)
+			self.setPos(tempPosX, tempPosY)
+			# print('airCondition', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'TV'):
+			direction = random.randint(1, 4)
+			if(direction == 1):
+				tempPosX = roomLeft
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 2):
+				tempPosX = roomRight
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 3):
+				tempPosY = roomTop
+				tempPosX = random.randint(roomLeft, roomRight)
+			if(direction == 4):
+				tempPosY = roomBottom
+				tempPosX = random.randint(roomLeft, roomRight)
+			self.setPos(tempPosX, tempPosY)
+			# print('TV', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'computer'):
+			tempPosX = random.randint(roomLeft, roomRight)
+			tempPosY = random.randint(roomTop, roomBottom)
+			self.setPos(tempPosX, tempPosY)
+			# print('computer', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'charger'):
+			tempPosX = random.randint(roomLeft, roomRight)
+			tempPosY = random.randint(roomTop, roomBottom)
+			self.setPos(tempPosX, tempPosY)
+			# print('charger', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'heater'):
+			tempPosX = random.randint(roomLeft, roomRight)
+			tempPosY = random.randint(roomTop, roomBottom)
+			self.setPos(tempPosX, tempPosY)
+			# print('heater', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'door'):
+			direction = random.randint(1, 4)
+			if(direction == 1):
+				tempPosX = roomLeft
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 2):
+				tempPosX = roomRight
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 3):
+				tempPosY = roomTop
+				tempPosX = random.randint(roomLeft, roomRight)
+			if(direction == 4):
+				tempPosY = roomBottom
+				tempPosX = random.randint(roomLeft, roomRight)
+			self.setPos(tempPosX, tempPosY)
+			# print('door', self.getPosX(), self.getPosY())
+		if(self.deviceType == 'window'):
+			direction = random.randint(1, 4)
+			tempPosX = int( (roomLeft+roomRight)/2 )
+			tempPosY = int( (roomTop+roomBottom)/2 )
+			if(direction == 1):
+				tempPosX = roomLeft
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 2):
+				tempPosX = roomRight
+				tempPosY = random.randint(roomTop, roomBottom)
+			if(direction == 3):
+				tempPosY = roomTop
+				tempPosX = random.randint(roomLeft, roomRight)
+			if(direction == 4):
+				tempPosY = roomBottom
+				tempPosX = random.randint(roomLeft, roomRight)
+			self.setPos(tempPosX, tempPosY)
+			# print('window',  self.getPosX(), self.getPosY())
+		if(self.deviceType == 'other'):
+			tempPosX = random.randint(roomLeft, roomRight)
+			tempPosY = random.randint(roomTop, roomBottom)
+			self.setPos(tempPosX, tempPosY)
+			# print('other:	', self.getPosX(), self.getPosY())
+
+	def getTypeAbbr(self):
+		if(self.deviceType == 'computer'):
+			return 'P'
+		return self.deviceType[0]
+
+	# 从数据库中读取数据（待验证）
+	def readInfoFromDB(self, ID = None, sqlMana = None):
+		if(ID == None or sqlMana == None):
+			return False
+		else:
+			# 读数据（待验证）
+			sql = 'select * from device where ID = %d' %ID
+			res = sqlMana.query(sql)
+			if(len(res) < 1):
+				return False
+			else:
+				temp = res[0]
+				self.ID         = temp.get("ID")
+				self.deviceType = temp.get("deviceType")
+				self.name       = str(temp.get("name"))
+				self.statu      = temp.get("statu")
+				self.value      = temp.get("value")
+				self.posX       = temp.get("posX")				
+				self.posY       = temp.get("posY")
+				self.code       = temp.get("code")
+				self.imageOn    = pygame.image.load('./pic/' + self.getType() + '_on.png')
+				self.imageOff   = pygame.image.load('./pic/' + self.getType() + '_off.png')
+				return True
+
+
+	# 向数据库中存储该设备信息
+	def saveInfoToDB(self, roomID = None, sqlMana = None):
+		if(roomID == None or sqlMana == None):
+			return False
+		else:
+			# 存数据
+			sql = 'select * from device where ID = %d' %self.ID
+			res = sqlMana.query(sql)
+			if(len(res) > 0):
+				sql = "update device set roomID= %d, deviceType = '%s', name = '%s', statu = %d, value = %lf, posX = %lf, posY = %lf, code = %d where ID = %d" %(roomID, self.deviceType, self.name, self.statu, self.value, self.posX, self.posY, self.code, self.ID)
+				sqlMana.update(sql)
+			else:
+				sql = "insert into device (roomID, deviceType, name, statu, value, posX, posY, code) values (%d, '%s', '%s', %d, %lf, %lf, %lf, %d)" %(roomID, self.deviceType, self.name, self.statu, self.value, self.posX, self.posY, self.code) 
+				sqlMana.insert(sql)
+				temp = sqlMana.query('select @@identity as newID')
+				self.ID = temp[0].get('newID')
+			return True
+
+	# 获取设备信息，用于保存记录
+	def getInfo(self):
+		tempStr = 'device_%03d_' %self.code
+		res = {
+			tempStr + 'ID'    : int(self.ID),
+			tempStr + 'statu' : int(self.statu),
+			tempStr + 'value' : float(self.value)
+		}
+		return res
+
+		
---
+++ rooms.py
@@ -0,0 +1,476 @@
+# -*- coding: utf-8 -*-
+import time
+from devices import device
+from light import lightSimulator
+from sqlManage import SQLManagement
+
+class room:
+
+
+	roomDict    =   {
+		"livingRoom"           :           0,
+		"masterBedroom"        :           100,
+		"secondBedroom"        :           200,
+		"thirdBedroom"         :           300,
+		"bathroom"             :           400,
+		"kitchen"              :           500,
+		"diningRoom"           :           600,
+		"studyRoom"            :           700,
+        "dormitory"            :           1000
+	}
+
+
+	def __init__(self, ID = -1, roomType = 'None', roomLeft = 0, roomRight = 0, roomTop = 0, roomBottom = 0):
+		# print('in __init__ of room')
+		
+        
+		# self.roomType   = self.roomDict.get(roomType)
+		self.ID         = ID
+		self.setType(roomType)
+		self.roomLeft   = roomLeft
+		self.roomRight  = roomRight
+		self.roomTop    = roomTop
+		self.roomBottom = roomBottom
+		self.deviceList = []
+		self.simL       = lightSimulator()
+		# self.initDeviceList()
+
+	# def antiRoomDict(self, roomType):
+	# 	for i in roomDict:
+	# 		if (roomType == self.roomDict.get(i)):
+	# 			return i
+
+	def printInfo(self):
+		print("Room type:	", self.roomType)
+		print("Room left:	", self.roomLeft)
+		print("Room right:	", self.roomRight)
+		print("Room top:	", self.roomTop)
+		print("Room bottom:	", self.roomBottom)
+
+	def getID(self):
+		return self.ID
+
+	def isInRoom(self, posX, posY):
+		return ( posX <= self.roomRight and  posX >= self.roomLeft and posY <= self.roomBottom and posY >= self.roomTop )
+
+	def getLeft(self):
+		return self.roomLeft
+
+	def setLeft(self, roomLeft):
+		self.roomLeft = roomLeft
+
+	def getRight(self):
+		return self.roomRight
+
+	def setRight(self, roomRight):
+		self.roomRight = roomRight
+
+	def getBottom(self):
+		return self.roomBottom
+
+	def setBottom(self, roomBottom):
+		self.roomBottom = roomBottom
+
+	def getTop(self):
+		return self.roomTop
+
+	def setTop(self, roomTop):
+		self.roomTop = roomTop
+
+	def getType(self):
+		return self.roomType
+
+	def setType(self, roomType):
+		if (self.roomDict.get(roomType) == None):
+			self.roomType = None
+		else:
+			self.roomType = roomType
+		# self.initDeviceList();
+
+	def getSquare(self):
+		dx = self.roomRight  - self.roomLeft + 1
+		dy = self.roomBottom - self.roomTop  + 1
+		square = dx * dy
+		return square
+
+	def getDeviceList(self):
+		return self.deviceList
+
+	def initDeviceList(self):
+		# print('in initDeviceList:	', self.getType())
+		# Livingroom
+		if (self.roomType == 'livingRoom'):
+			livingRoomTV             = device(0, 'livingRoomTV', 'TV', 0, 0)
+			self.deviceList.append(livingRoomTV)
+
+			livingRoomLamp           = device(1, 'livingRoomLamp', 'lamp', 0, 0)
+			self.deviceList.append(livingRoomLamp)
+
+			livingRoomDoor           = device(2, 'livingRoomDoor', 'door', 0, 0)
+			self.deviceList.append(livingRoomDoor)
+
+			livingRoomWindow         = device(3, 'livingRoomWindow', 'window', 0, 0)
+			self.deviceList.append(livingRoomWindow)
+
+			livingRoomComputer       = device(4, 'livingRoomComputer', 'computer', 0, 0)
+			self.deviceList.append(livingRoomComputer)
+
+			livingRoomCharger        = device(5, 'livingRoomCharger', 'charger', 0, 0)
+			self.deviceList.append(livingRoomCharger)
+
+			livingRoomAirCondition   = device(6, 'livingRoomAirCondition', 'airCondition', 0, 25)
+			self.deviceList.append(livingRoomAirCondition)
+
+			livingRoomAudioSystem    = device(7, 'livingRoomAudioSystem', 'other', 0, 0)
+			self.deviceList.append(livingRoomAudioSystem)
+
+		# Master Bedroom
+		if (self.roomType == 'masterBedroom'):
+			masterBedRoomTV          = device(100, 'masterBedRoomTV', 'TV', 0, 0)
+			self.deviceList.append(masterBedRoomTV)
+
+			masterBedRoomCeilingLamp = device(101, 'masterBedRoomCeilingLamp', 'lamp', 0, 0)
+			self.deviceList.append(masterBedRoomCeilingLamp)
+
+			masterBedRoomBedLamp     = device(102, 'masterBedRoomBedLamp', 'lamp', 0, 0)
+			self.deviceList.append(masterBedRoomBedLamp)
+
+			masterBedRoomDoor        = device(103, 'masterBedRoomDoor', 'door', 0, 0)
+			self.deviceList.append(masterBedRoomDoor)
+
+			masterBedRoomWindow      = device(104, 'masterBedRoomWindow', 'window', 0, 0)
+			self.deviceList.append(masterBedRoomWindow)
+
+			masterBedRoomAirCondition= device(105, 'masterBedRoomAirCondition', 'airCondition', 0, 25)
+			self.deviceList.append(masterBedRoomAirCondition)
+
+			masterBedRoomComputer    = device(106, 'masterBedRoomComputer', 'computer', 0, 0)
+			self.deviceList.append(masterBedRoomComputer)
+
+			masterBedRoomCharger     = device(107, 'masterBedRoomCharger', 'charger', 0, 0)
+			self.deviceList.append(masterBedRoomCharger)
+
+		# Second Bedroom
+		if (self.roomType == 'secondBedroom'):
+			secondBedRoomTV          = device(200, 'secondBedRoomTV', 'TV', 0, 0)
+			self.deviceList.append(secondBedRoomTV)
+
+			secondBedRoomCeilingLamp = device(201, 'secondBedRoomCeilingLamp', 'lamp', 0, 0)
+			self.deviceList.append(secondBedRoomCeilingLamp)
+
+			secondBedRoomBedLamp     = device(202, 'secondBedRoomBedLamp', 'lamp', 0, 0)
+			self.deviceList.append(secondBedRoomBedLamp)
+
+			secondBedRoomDoor        = device(203, 'secondBedRoomDoor', 'door', 0, 0)
+			self.deviceList.append(secondBedRoomDoor)
+
+			secondBedRoomWindow      = device(204, 'secondBedRoomWindow', 'window', 0, 0)
+			self.deviceList.append(secondBedRoomWindow)
+
+			secondBedRoomAirCondition= device(205, 'secondBedRoomAirCondition', 'airCondition', 0, 25)
+			self.deviceList.append(secondBedRoomAirCondition)
+
+			secondBedRoomComputer    = device(206, 'secondBedRoomComputer', 'computer', 0, 0)
+			self.deviceList.append(secondBedRoomComputer)
+
+			secondBedRoomCharger     = device(207, 'secondBedRoomCharger', 'charger', 0, 0)
+			self.deviceList.append(secondBedRoomCharger)
+
+		# Third Bedroom
+		if (self.roomType == 'thirdBedroom'):
+			thirdBedRoomTV          = device(300, 'thirdBedRoomTV', 'TV', 0, 0)
+			self.deviceList.append(thirdBedRoomTV)
+
+			thirdBedRoomCeilingLamp = device(301, 'thirdBedRoomCeilingLamp', 'lamp', 0, 0)
+			self.deviceList.append(thirdBedRoomCeilingLamp)
+
+			thirdBedRoomBedLamp     = device(302, 'thirdBedRoomBedLamp', 'lamp', 0, 0)
+			self.deviceList.append(thirdBedRoomBedLamp)
+
+			thirdBedRoomDoor        = device(303, 'thirdBedRoomDoor', 'door', 0, 0)
+			self.deviceList.append(thirdBedRoomDoor)
+
+			thirdBedRoomWindow      = device(304, 'thirdBedRoomWindow', 'window', 0, 0)
+			self.deviceList.append(thirdBedRoomWindow)
+
+			thirdBedRoomAirCondition= device(305, 'thirdBedRoomAirCondition', 'airCondition', 0, 25)
+			self.deviceList.append(thirdBedRoomAirCondition)
+
+			thirdBedRoomComputer    = device(306, 'thirdBedRoomComputer', 'computer', 0, 0)
+			self.deviceList.append(thirdBedRoomComputer)
+
+			thirdBedRoomCharger     = device(307, 'thirdBedRoomCharger', 'charger', 0, 0)
+			self.deviceList.append(thirdBedRoomCharger)
+
+		# Bathroom
+		if (self.roomType == 'bathroom'):
+			bathroomLamp            = device(400, 'bathroomLamp', 'lamp', 0, 0)
+			self.deviceList.append(bathroomLamp)
+
+			bathroomWaterHeater     = device(401, 'bathroomWaterHeater', 'heater', 0, 0)
+			self.deviceList.append(bathroomWaterHeater)
+
+			bathroomDoor            = device(402, 'bathroomDoor', 'door', 0, 0)
+			self.deviceList.append(bathroomDoor)
+
+			bathroomWindow          = device(403, 'bathroomWindow', 'window', 0, 0)
+			self.deviceList.append(bathroomWindow)
+
+			bathroomCharger         = device(404, 'bathroomCharger', 'charger', 0, 0)
+			self.deviceList.append(bathroomCharger)
+
+			bathroomFan             = device(405, 'bathroomFan', 'other', 0, 0)
+			self.deviceList.append(bathroomFan)
+
+		# Kitchen	
+		if (self.roomType == 'kitchen'):
+			kitchenLamp             = device(500, 'kitchenLamp', 'lamp', 0, 0)
+			self.deviceList.append(kitchenLamp)
+
+			kitchenDoor             = device(501, 'kitchenDoor', 'door', 0, 0)
+			self.deviceList.append(kitchenDoor)
+
+			kitchenWindow           = device(502, 'kitchenWindow', 'window', 0, 0)
+			self.deviceList.append(kitchenWindow)
+
+			kitchenVentilator       = device(503, 'kitchenVentilator', 'other', 0, 0)
+			self.deviceList.append(kitchenVentilator)
+
+		# Dining Room	
+		if (self.roomType == 'diningRoom'):
+			diningRoomLamp          = device(600, 'diningRoomLamp', 'lamp', 0, 0)
+			self.deviceList.append(diningRoomLamp)
+
+			diningRoomDoor          = device(601, 'diningRoomDoor', 'door', 0, 0)
+			self.deviceList.append(diningRoomDoor)
+
+			diningRoomFan           = device(602, 'diningRoomFan', 'other', 0, 0)
+			self.deviceList.append(diningRoomFan)
+
+			diningRoomWindow        = device(603, 'diningRoomWindow', 'window', 0, 0)
+			self.deviceList.append(diningRoomWindow)
+
+			diningRoomAirCondition  = device(604, 'diningRoomAirCondition', 'airCondition', 0, 0)
+			self.deviceList.append(diningRoomAirCondition)
+
+		# Study Room
+		if (self.roomType == 'studyRoom'):
+			studyRoomCeilingLamp    = device(700, 'studyRoomCeilingLamp', 'lamp', 0, 0)
+			self.deviceList.append(studyRoomCeilingLamp)
+
+			studyRoomDeskLamp       = device(701, 'studyRoomDeskLamp', 'lamp', 0, 0)
+			self.deviceList.append(studyRoomDeskLamp)
+
+			studyRoomAirCondition   = device(702, 'studyRoomAirCondition', 'airCondition', 0, 0)
+			self.deviceList.append(studyRoomAirCondition)
+
+			studyRoomDoor           = device(703, 'studyRoomDoor', 'door', 0, 0)
+			self.deviceList.append(studyRoomDoor)
+
+			studyRoomWindow         = device(704, 'studyRoomWindow', 'window', 0, 0)
+			self.deviceList.append(studyRoomWindow)
+
+			studyRoomComputer       = device(705, 'studyRoomComputer', 'computer', 0, 0)
+			self.deviceList.append(studyRoomComputer)
+
+			studyRoomCharger        = device(706, 'studyRoomCharger', 'charger', 0, 0)
+			self.deviceList.append(studyRoomCharger)
+
+			studyRoomAudioSystem    = device(707, 'studyRoomAudioSystem', 'other', 0, 0)
+			self.deviceList.append(studyRoomAudioSystem)
+            
+            
+        #dormitory
+		if (self.roomType == 'dormitory'):
+            #小爱音箱*5
+			dormitoryXiaoaiAudio1 = device(1001, 'dormitoryXiaoaiAudio1', 'audio', 0, 0)
+			self.deviceList.append(dormitoryXiaoaiAudio1)
+			dormitoryXiaoaiAudio2 = device(1002, 'dormitoryXiaoaiAudio2', 'audio', 0, 0)
+			self.deviceList.append(dormitoryXiaoaiAudio2)
+			dormitoryXiaoaiAudio3 = device(1003, 'dormitoryXiaoaiAudio3', 'audio', 0, 0)
+			self.deviceList.append(dormitoryXiaoaiAudio3)
+			dormitoryXiaoaiAudio4 = device(1004, 'dormitoryXiaoaiAudio4', 'audio', 0, 0)
+			self.deviceList.append(dormitoryXiaoaiAudio4)
+			dormitoryXiaoaiAudio5 = device(1005, 'dormitoryXiaoaiAudio5', 'audio', 0, 0)
+			self.deviceList.append(dormitoryXiaoaiAudio5)
+            #蓝牙温湿度计
+			dormitoryThermometer = device(1006, 'dormitoryThermometer', 'thermometer', 0, 0)
+			self.deviceList.append(dormitoryThermometer)
+            #摄像机
+			dormitoryCamere1 = device(1007, 'dormitoryCamere1', 'camere', 0, 0)
+			self.deviceList.append(dormitoryCamere1)
+            #门窗传感器*3
+			dormitoryDoorWindowSensor1 = device(1008, 'dormitoryDoorWindowSensor1', 'sensor', 0, 0)
+			self.deviceList.append(dormitoryDoorWindowSensor1)
+			dormitoryDoorWindowSensor2 = device(1009, 'dormitoryDoorWindowSensor2', 'sensor', 0, 0)
+			self.deviceList.append(dormitoryDoorWindowSensor2)
+			dormitoryDoorWindowSensor3 = device(1010, 'dormitoryDoorWindowSensor3', 'sensor', 0, 0)
+			self.deviceList.append(dormitoryDoorWindowSensor3)
+            #人体移动传感器*2
+			dormitoryHumanSensor1 = device(1011, 'dormitoryHumanSensor1', 'sensor', 0, 0)
+			self.deviceList.append(dormitoryHumanSensor1)   
+			dormitoryHumanSensor2 = device(1012, 'dormitoryHumanSensor2', 'sensor', 0, 0)
+			self.deviceList.append(dormitoryHumanSensor2)
+            #三开关单控开关
+			dormitoryThreeSwitch = device(1013, 'dormitoryThreeSwitch', 'switch', 0, 0)
+			self.deviceList.append(dormitoryThreeSwitch)  
+            #窗帘伴侣
+			dormitoryCurtainCompanion = device(1014, 'dormitoryCurtainCompanion', 'switch', 0, 0)
+			self.deviceList.append(dormitoryCurtainCompanion) 
+            #智能插座
+			dormitorySmartSocket = device(1015, 'dormitorySmartSocket', 'socket', 0, 0)
+			self.deviceList.append(dormitorySmartSocket) 
+
+            
+		self.setDevicesPosRandom()
+
+
+	def setDevicesPosRandom(self):
+		for tempDevice in self.deviceList:
+			tempDevice.setRandomDevicePos(self.roomLeft, self.roomRight, self.roomTop, self.roomBottom)
+
+	# def changeDeviceByType(self, deviceType, newStatu, newValue):
+	# 	for tempDevice in self.deviceList:
+	# 		if(tempDevice.getType() == deviceType):
+
+	# 开启指定设备类型的设备，返回指定类型的设备列表
+	def turnOnDeviceByType(self, deviceType):
+		resDeviceList = []
+		for tempDevice in self.deviceList:
+			if ( tempDevice.getType() == deviceType ):
+				tempDevice.turnOn()
+				resDeviceList.append(tempDevice)
+				if(tempDevice.getType() == 'lamp'):
+					self.simL.setLightOn()
+		return resDeviceList
+
+	# 关闭指定设备类型的设备，返回指定类型的设备列表
+	def turnOffDeviceByType(self, deviceType):
+		resDeviceList = []
+		for tempDevice in self.deviceList:
+			if ( tempDevice.getType() == deviceType ):
+				tempDevice.turnOff()
+				resDeviceList.append(tempDevice)
+				if(tempDevice.getType() == 'lamp'):
+					self.simL.setLightOff()
+		return resDeviceList
+
+	# 开启制定设备名的设备，返回对应设备
+	def turnOnDeviceByName(self, deviceName):
+		for tempDevice in self.deviceList:
+			if ( tempDevice.getName() == deviceName ):
+				tempDevice.turnOn()
+				if(tempDevice.getType() == 'lamp'):
+					self.simL.setLightOn()
+				return tempDevice
+		return None
+
+	# 关闭制定设备名的设备，返回对应设备
+	def turnOffDeviceByName(self, deviceName):
+		for tempDevice in self.deviceList:
+			if( tempDevice.getName() == deviceName ):
+				tempDevice.turnOff()
+				if(tempDevice.getType() == 'lamp'):
+					self.simL.setLightOff()
+				return tempDevice
+		return None
+
+	# 设置制定设备类型的设备的值，返回指定类型的设备列表
+	def setDeviceValueByType(self, deviceType, value):
+		resDeviceList = []
+		for tempDevice in self.deviceList:
+			if ( tempDevice.getType() == deviceType ):
+				tempDevice.setValue(value)
+				resDeviceList.append(tempDevice)
+		return resDeviceList
+    
+	def turnOffDeviceByTypeAndOwner(self, deviceName, deviceType, deviceID):
+		for tempDevice in self.deviceList:
+			if( tempDevice.getName() == deviceName and tempDevice.getID() == deviceID):
+				tempDevice.turnOff()
+				if(tempDevice.getType() == 'lamp'):
+					self.simL.setLightOff()
+				return tempDevice
+		return None
+        
+	# 获取房间当前亮度
+	def getLight(self, currentTime):
+		return self.simL.getCurrentLight(currentTime)
+
+	# 房间是否黑暗
+	def isDarkness(self, currentTime):
+		return self.simL.isDarkness(currentTime)
+
+	# 获取房间亮度百分比
+	def getLightPercentage(self, currentTime):
+		return self.simL.getPercentage(currentTime)
+
+	# 存储房间信息到数据库
+	def saveInfoToDB(self, houseID = None, sqlMana = None):
+		if(houseID == None or sqlMana == None):
+			return False
+		else:
+			# 存储数据
+			flag = True
+			sql = 'select * from room where ID = %d' %self.ID
+			res = sqlMana.query(sql)
+			if(len(res) > 0):
+				sql = "update room set houseID= %d, roomType = '%s', roomLeft = %lf, roomRight = %lf, roomTop = %lf, roomBottom = %lf where ID = %d" %(houseID, self.roomType, self.roomRight, self.roomTop, self.roomBottom, self.ID)
+				sqlMana.update(sql)
+			else:
+				sql = "insert into room (houseID, roomType, roomLeft, roomRight, roomTop, roomBottom) values (%d, '%s', %lf, %lf, %lf, %lf)" %(houseID, self.roomType, self.roomLeft, self.roomRight, self.roomTop, self.roomBottom) 
+				# print("sdfsdfsdfsfdfssfsfd: ", sql)
+				sqlMana.insert(sql)
+				temp = sqlMana.query('select @@identity as newID')
+				self.ID = temp[0].get('newID')
+			for tempDevice in self.deviceList:
+				flag = tempDevice.saveInfoToDB(self.ID, sqlMana) and flag
+			return flag
+
+	# 从数据库中读取房间信息（待验证）
+	def readInfoFromDB(self, ID = None, sqlMana = None):
+		if(ID == None or sqlMana == None):
+			return False
+		else:
+			# 读数据（待验证）
+			sql = 'select * from room where ID = %d' %ID
+			res = sqlMana.query(sql)
+			if(len(res) < 1):
+				return False
+			else:
+				temp = res[0]
+				self.ID         = int(temp.get("ID"))
+				self.roomType   = str(temp.get("roomType"))
+				self.roomLeft   = float(temp.get("roomLeft"))
+				self.roomRight  = float(temp.get("roomRight"))
+				self.roomTop    = float(temp.get("roomTop"))
+				self.roomBottom = float(temp.get("roomBottom"))
+				self.simL       = lightSimulator()
+				self.deviceList = []
+				self.initDeviceListFromDB(sqlMana)
+				return True
+
+	# 通过数据库初始化设备列表
+	def initDeviceListFromDB(self, sqlMana = None):
+		sql = 'select ID from device where roomID = %d' %self.ID
+		res = sqlMana.query(sql)
+		for temp in res:
+			tempDevice = device()
+			tempDevice.readInfoFromDB(temp.get('ID'), sqlMana)
+			self.deviceList.append(tempDevice)
+
+	# 获取房间全部信息，用于保存记录
+	def getInfo(self, currentTime):
+		tempCode = int(self.roomDict.get(self.roomType) / 100)
+		tempStr = "light_%d" %tempCode
+		tempLight = self.simL.getCurrentLight(currentTime)
+		res = {
+			tempStr : tempLight
+		}
+		for tempDevice in self.deviceList:
+			res = dict( res.items() + tempDevice.getInfo().items() )
+		return res
+
+
+    
+
